a86ff5dc9e0132dddf7d6c734a6c84e42a677516
Vikram <vikram@spotflik.com>
2018-10-04 16:06:45 +0530

    Added- Bootstrap vue, vuesax, sass/scss

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+Vue.use(Vuesax)
 
 /* eslint-disable no-new */
 new Vue({
